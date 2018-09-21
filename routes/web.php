5bd4730906e68fe8f0801e0d7f666faf19835fea
Adil Chbada <nanoadil@gmail.com>
2018-09-21 10:07:07 +0100

    fix err

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Route::group(['prefix'=>'lManager'],function (){
-    Route::get('/','Adilchbada\LaravelManager\Http\Controllers\AppManagerController@index');
-    Route::post('/exec','Adilchbada\LaravelManager\Http\Controllers\AppManagerController@exec')->name('laravel_manager_exec');
-
-});
-
